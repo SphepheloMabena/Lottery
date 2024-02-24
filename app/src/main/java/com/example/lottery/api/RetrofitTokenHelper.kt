e78c3ce28880d5e84b49e225a509955601fb62a2
Sphephelo Mabena <sphephelomabena@gmail.com>
2024-02-24 18:30:09 +0200

    Added Retrofit Token Helper

---
+++ app/src/main/java/com/example/lottery/api/RetrofitTokenHelper.kt
@@ -0,0 +1,16 @@
+package com.example.lottery.api
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+
+class RetrofitTokenHelper {
+
+    private val baseUrl = "https://lotteryapp.onrender.com"
+
+    fun getInstance(): Retrofit {
+        return Retrofit.Builder().baseUrl(baseUrl)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+}
